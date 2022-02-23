2ee898efe0359bf995c03ca044e741e93164c074
Simone823 <simone.daglio823@outlook.it>
2022-02-23 16:38:05 +0100

    stampo messaggio e immagine vuejs

---
+++ app.js
@@ -0,0 +1,12 @@
+// Vue js container html
+const container = new Vue({
+    
+    // Container div html 
+    el: "#container",
+
+    // Variabili 
+    data: {
+        message: "Ciao",
+        imgRandom: "https://picsum.photos/200/300",
+    }  
+})
